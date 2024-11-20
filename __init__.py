e97b519f0503ef5980de83a31d77aaf5be66e33b
sjad92 <120110304+sjad92@users.noreply.github.com>
2024-11-20 15:26:04 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -30,6 +30,18 @@ def meteo():
 @app.route("/rapport/")
 def mongraphique():
     return render_template("graphique.html")
+
+@app.route('/dino/')
+def meteo():
+    response = urlopen('https://samples.openweathermap.org/data/2.5/forecast?lat=0&lon=0&appid=xxx')
+    raw_content = response.read()
+    json_content = json.loads(raw_content.decode('utf-8'))
+    results = []
+    for list_element in json_content.get('list', []):
+        dt_value = list_element.get('dt')
+        temp_day_value = list_element.get('main', {}).get('temp') - 273.15 # Conversion de Kelvin en °c 
+        results.append({'Jour': dt_value, 'temp': temp_day_value})
+    return jsonify(results=results)
   
 if __name__ == "__main__":
   app.run(debug=True)
